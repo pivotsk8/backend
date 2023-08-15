f7c4b628c0127542e0ea99009e3337335092bbe1
piiivot <the_pivotsk8@hotmail.com>
2023-08-15 11:19:05 -0500

    feat: created function for add seeders

---
+++ data/seed.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { db } from '../config/db.js'
+import Services from '../models/Services.js'
+import { services } from './beautyServices.js'
+
+dotenv.config()
+await db()
+
+async function seedDB() {
+    try {
+        await Services.insertMany(services)
+        console.log(colors.green.bold('los servicos fueron creados'))
+        process.exit()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+async function clearDB() {
+    console.log('clear')
+}
+
+process.argv[2] === '--import' ? seedDB() : clearDB()

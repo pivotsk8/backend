56b0327fc0ef1c3eab5185ede7a8f5d1194831d7
piiivot <the_pivotsk8@hotmail.com>
2023-09-28 10:40:12 -0500

    feat: send to front view and env variable

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -2,21 +2,22 @@ import { createTransport } from '../config/nodemailer.js'
 
 export async function sendEmailVerification({ name, email, token }) {
     const transporter = createTransport(
-        "sandbox.smtp.mailtrap.io",
-        2525,
-        "51cf12e4496159",
-        "4b5c155a6c56b3"
+        process.env.EMAIL_HOST,
+        process.env.EMAIL_PORT,
+        process.env.EMAIL_USER,
+        process.env.EMAIL_PASS
+
     )
 
     //Enviar el email
     const info = await transporter.sendMail({
-        from: 'AppSalon',
+        from: 'AppSalon <cuentas@appsalon.com>',
         to: 'correo@.correo.com',
         subject: 'AppSalon -Confirma tu cuenta',
         text: 'AppSalon - Confirma tu cuenta',
         html: `<p>Hola: ${name}, confirma tu cuenta en AppSalon</p>
         <p>Tu cuenta esta casi lista, solo debes confirmarla en el siguiente enlace</p>
-        <a href="http://localhost:4000/api/auth/verify/${token}">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
         <p>Si tu no create esta cuenta, puedes ignorar este mensaje</p>`
     })
 

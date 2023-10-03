a84ed6f846c533b58d69d34b71231681fa949188
piiivot <the_pivotsk8@hotmail.com>
2023-10-03 10:55:56 -0500

    feat: response

--- controlers/appointmentContoller.js
+++ controlers/appointmentContoller.js
@@ -3,11 +3,12 @@ import Appointment from '../models/Appointment.js'
 const createAppointment = async (req, res) => {
     const appointment = req.body
     appointment.user = req.user._id.toString()
-    console.log(appointment)
-
     try {
         const newAppointment = new Appointment(appointment)
         await newAppointment.save()
+        res.json({
+            msg: 'Cita Almacenada Correctamente'
+        })
     } catch (error) {
         console.log('error')
     }

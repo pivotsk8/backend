451a17536a177fe8d2e11317a092598b7e7c5a19
piiivot <the_pivotsk8@hotmail.com>
2023-10-05 14:34:06 -0500

    feat protected route four user

--- controlers/userController.js
+++ controlers/userController.js
@@ -1,5 +1,24 @@
+import Appointment from '../models/Appointment.js'
+
 const getUserAppointments = async (req, res) => {
-    console.log('desde yo')
+    const { user } = req.params
+
+    if (user !== req.user._id.toString()) {
+        const error = new Error('Acceso Denegado')
+        return res.status(400).json({ msg: error.message })
+    }
+
+    try {
+        const appointment = await Appointment.find({
+            user,
+            date: {
+                $gte: new Date()
+            }
+        }).populate('services')
+        res.json(appointment)
+    } catch (error) {
+
+    }
 }
 
 export {
